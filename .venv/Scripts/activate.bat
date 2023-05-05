5ce7d47d2c9695fc329ddafc671525198ae73e3c
Theo <theomhalpern@gmail.com>
2023-05-05 12:33:30 -0700

    added code for map page

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Arkar K Swar\Documents\Coding\WebDev\msdocs-python-flask-webapp-quickstart\.venv
+set VIRTUAL_ENV=C:\Users\theom\Documents\GitHub\WildTrek\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(.venv) 
 
 :END
 if defined _OLD_CODEPAGE (
