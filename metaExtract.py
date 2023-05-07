7b43d350ec6b50b827968d3c7c07c95c821c86bd
Theo <theomhalpern@gmail.com>
2023-05-07 09:59:14 -0700

    got rid of images

--- metaExtract.py
+++ metaExtract.py
@@ -1,6 +1,6 @@
 import os
 
-os.chdir('C://Users/theom/Documents/GitHub/WildTrek/static/images/carwithMeta.jpg')
+os.chdir('C://Users/theom/Documents/GitHub/WildTrek/https://wildtrekimages.theohal.repl.co/images/carwithMeta.jpg')
 image_list = os.listdir()
 image_list = [a for a in image_list if a.endswith('jpg')]
 
